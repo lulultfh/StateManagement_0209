a2a41bd4ef4d1d33b64d0b5b2439f4cdeddbe323
lulultfh <punyalulu05@gmail.com>
2026-04-03 12:23:07 +0700

    feat: menambahkan event NameChanged dengan properti nama dan EmailChanged dengan properti email

--- lib/bloc/blocku.dart
+++ lib/bloc/blocku.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 
 abstract class FormEvent {}
 
+
+class NameChanged extends FormEvent{
+  final String nama;
+  NameChanged(this.nama);
+}
+
+
+class EmailChanged extends FormEvent{
+  final String email;
+  EmailChanged(this.email);
+}
+
+
 class FormPage4 extends StatelessWidget {
   const FormPage4({super.key});
 
