b74099e6645ef0c31bfd278c979e5b247c51cd7b
lulultfh <punyalulu05@gmail.com>
2026-04-03 17:10:52 +0700

    feat: menambahkan halaman form dengan input nama dan email serta integrasi FormProvider

--- lib/provider.dart
+++ lib/provider.dart
@@ -33,3 +33,37 @@ class FormProvider extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+// ui
+class FormPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final form = Provider.of<FormProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Form Provider")),
+      body: Padding(padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: "Nama"),
+            onChanged: form.setNama,
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: "Email"),
+            onChanged: form.setEmail,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(onPressed: (){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("Submit sukses!"))
+            );
+          }, child: Text("Submit")),
+          SizedBox(height: 10),
+          Text("Nama: ${form.nama}"),
+          Text("Email: ${form.email}"),
+        ],
+      ),),
+    );
+  }
+}
