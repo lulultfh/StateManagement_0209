658e4eb004d565b4fbe1ef54ce1283da4cdb0986
lulultfh <punyalulu05@gmail.com>
2026-04-03 16:27:43 +0700

    feat: menambahkan MyApp class sebagai turunan dari StatelessWidget dan FormPage sebagai halaman utama

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,11 +2,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
-import 'package:statemanagement/main.dart';
 
 final namaProvider = StateProvider<String>((ref) => "");
 final emailProvider = StateProvider<String>((ref) => "");
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FormPage(),
+    );
+  }
 }
