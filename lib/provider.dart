037a564f7d997594effd5620ec4cc4d88066a8be
lulultfh <punyalulu05@gmail.com>
2026-04-03 17:05:24 +0700

    feat: menambahkan ChangeNotifierProvider untuk mengelola state FormProvider

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main(){
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => FormProvider(),
+      child: MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 //model
 class FormProvider extends ChangeNotifier {
