bdf11ae93d435ce70abd2866ce1278b49b47674a
lulultfh <punyalulu05@gmail.com>
2026-04-03 16:24:59 +0700

    feat: menambahkan provider scope pada root app untuk mengaktifkan riverpod

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,5 +1,12 @@
 // provider
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:statemanagement/main.dart';
 
 final namaProvider = StateProvider<String>((ref) => "");
 final emailProvider = StateProvider<String>((ref) => "");
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
