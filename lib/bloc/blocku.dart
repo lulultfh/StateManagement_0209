b17869e7ed58179eb794406fe66d52de7ef9c5af
lulultfh <punyalulu05@gmail.com>
2026-04-03 12:31:31 +0700

    feat: menambahkan state FormState untuk menangani inputan nama dan email

--- lib/bloc/blocku.dart
+++ lib/bloc/blocku.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 
 abstract class FormEvent {}
 
-
-class NameChanged extends FormEvent{
+class NameChanged extends FormEvent {
   final String nama;
   NameChanged(this.nama);
 }
 
-
-class EmailChanged extends FormEvent{
+class EmailChanged extends FormEvent {
   final String email;
   EmailChanged(this.email);
 }
 
+// state
+class FormState {
+  final String nama;
+  final String email;
+
+  FormState({this.nama = "", this.email = ""});
+}
 
 class FormPage4 extends StatelessWidget {
   const FormPage4({super.key});
